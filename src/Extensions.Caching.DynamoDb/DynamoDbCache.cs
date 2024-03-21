19213f8dd167f9cc8edde54733349b8cbb4e31a5
Ivan Biljan <ssbiljan@gmail.com>
2024-03-21 10:37:46 +0100

    Add DynamoDbCacheOptions

--- src/Extensions.Caching.DynamoDb/DynamoDbCache.cs
+++ src/Extensions.Caching.DynamoDb/DynamoDbCache.cs
@@ -1,5 +1,10 @@
-﻿namespace Extensions.Caching.DynamoDb;
+﻿using Microsoft.Extensions.Caching.Distributed;
 
-public class DynamoDbCache
+namespace Extensions.Caching.DynamoDb;
+
+/// <summary>
+///     Represents an implementation of <see cref="IDistributedCache" /> that uses DynamoDb as backing storage.
+/// </summary>
+public sealed class DynamoDbCache
 {
 }
---
+++ src/Extensions.Caching.DynamoDb/DynamoDbCacheOptions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Extensions.Caching.DynamoDb;
+
+/// <summary>
+///     Defines configuration options for the <see cref="DynamoDbCache" />.
+/// </summary>
+public sealed record DynamoDbCacheOptions
+{
+    /// <summary>
+    ///     Identifies the section used to bind these options from a configuration source.
+    /// </summary>
+    public const string ConfigurationSectionName = "DynamoDbCache";
+
+    /// <summary>
+    ///     Gets the AWS region.
+    /// </summary>
+    public string Region { get; init; } = string.Empty;
+
+    /// <summary>
+    ///     Gets the access key ID component of the access key pair used to authenticate requests towards AWS.
+    /// </summary>
+    public string? AccessKeyId { get; init; }
+
+    /// <summary>
+    ///     Gets the secret access key component of the access key pair used to authenticate requests towards AWS.
+    /// </summary>
+    public string? SecretKey { get; init; }
+
+    /// <summary>
+    ///     Gets the endpoint DynamoDb will connect to.
+    /// </summary>
+    public string? ServiceUrl { get; init; }
+}
+
+internal sealed class DynamoDbCacheOptionsValidator : IValidateOptions<DynamoDbCacheOptions>
+{
+    /// <inheritdoc />
+    public ValidateOptionsResult Validate(string? name, DynamoDbCacheOptions options)
+    {
+        var failures = new List<string>();
+        if (string.IsNullOrWhiteSpace(options.Region))
+        {
+            failures.Add("Region must not be empty");
+        }
+
+        if (!string.IsNullOrWhiteSpace(options.AccessKeyId) && string.IsNullOrWhiteSpace(options.SecretKey) ||
+            string.IsNullOrWhiteSpace(options.AccessKeyId) && !string.IsNullOrWhiteSpace(options.SecretKey))
+        {
+            failures.Add("Incomplete access key");
+        }
+
+        return failures.Count != 0 ? ValidateOptionsResult.Fail(failures) : ValidateOptionsResult.Success;
+    }
+}
